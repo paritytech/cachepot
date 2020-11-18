d7690f8bb3d3b010b0c26e5f3216563dc690b664
Bernhard Schuster <bernhard@ahoi.io>
2020-11-18 18:20:12 +0100

    chore: silence complexity warnings
    Remove once migrated to async/await

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(clippy::complexity)]
+#![deny(clippy::perf)]
+
 #![deny(rust_2018_idioms)]
 #![recursion_limit = "256"]
 
