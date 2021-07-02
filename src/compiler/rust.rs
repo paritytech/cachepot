90e1dc81079f507a60832ee8abcb9bb922bcfe87
Crystalin <alan@purestake.com>
2021-07-02 23:39:02 +0000

    Adds SUBSTRATE_ env variables to hash

--- src/compiler/rust.rs
+++ src/compiler/rust.rs
@@ -1378,7 +1378,9 @@ where
         env_vars.sort();
         for &(ref var, ref val) in env_vars.iter() {
             // CARGO_MAKEFLAGS will have jobserver info which is extremely non-cacheable.
-            if var.starts_with("CARGO_") && var != "CARGO_MAKEFLAGS" {
+            if (var.starts_with("CARGO_") && var != "CARGO_MAKEFLAGS")
+                || var.starts_with("SUBSTRATE_")
+            {
                 var.hash(&mut HashToDigest { digest: &mut m });
                 m.update(b"=");
                 val.hash(&mut HashToDigest { digest: &mut m });
